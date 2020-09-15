90a9dbe2dfde5a4912d4eb2c254a14d19182e6d9
daveKidd <david.kidd@lambdaschool.com>
2020-09-14 21:59:01 -0600

    finished

--- index.js
+++ index.js
@@ -14,10 +14,13 @@
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
 */
+let arrayOfStrings = ["Why", "Hello", "There"];
 function processFirstItem(stringList, callback) {
   return callback(stringList[0])
 }
-
+processFirstItem(arrayOfStrings,function(firstItem){
+  console.log(firstItem+firstItem);
+})
 // ⭐️ Example Challenge END ⭐️
 
 
@@ -26,12 +29,14 @@ function processFirstItem(stringList, callback) {
 /* Task 1: `counterMaker`
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
- * 1. What is the difference between counter1 and counter2?
+ * 1. What is the difference between counter1 and counter2? - counter1 would remember what count is.  counter2 wouldn't
  * 
- * 2. Which of the two uses a closure? How can you tell?
+ * 2. Which of the two uses a closure? How can you tell? - counter1 uses closure.  You can tell becaue there is a nested
+ * variable, count that reaches outward to add one to count.  It has a function nested in the other one.
  * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
- *
+ *  counter1 would be great if you didn't need to remember the value of count and only needed to do it to one item in 
+ * the code.  counter2 is great to apply to different functions and will remember what count is for each function
 */
 
 // counter1 code
@@ -56,11 +61,10 @@ function counter2() {
 
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning(){  
+  return Math.floor(Math.random() * 3);
 }
+
 
 /* Task 3: finalScore()
 
@@ -76,11 +80,24 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(cb,inn){
+  let scoreObj = {};
+  let homeTotal = 0;
+  let awayTotal = 0;
+  
+  for(i=0;i<=inn;i++){
+    let homeRandom = cb();
+    homeTotal += homeRandom;
+    let awayRandom = cb();
+    awayTotal += awayRandom;    
+  }
 
-  /*Code Here*/
-
+  scoreObj.Home = homeTotal;
+  scoreObj.Away = awayTotal;
+  console.log(scoreObj);
+  console.log("In " + inn + " innings the total score was Home: " + scoreObj.Home + " and Away: " + scoreObj.Away);  
 }
+finalScore(inning,9);
 
 /* Task 4: 
 
@@ -103,9 +120,28 @@ and returns the score at each pont in the game, like so:
 9th inning: awayTeam - homeTeam
 
 Final Score: awayTeam - homeTeam */
-
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function getInningScore(inn,numInn){
+  let totalAway = 0;
+  let totalHome = 0;
+  for(i=1;i<=numInn;i++){
+    totalHome += inn();
+    totalAway += inn();
+    console.log("Inning " + i + ": Away Team: " + totalAway + " - Home Team: " + totalHome);
+  }
+  if(totalAway > totalHome){
+    console.log("Away Team Wins!");
+  }
+  else if(totalHome > totalAway){
+    console.log("Home Team Wins!");
+  }
+  else{
+    console.log("It's a tie!");
+  }
 }
+function scoreboard(getInn, inn, numInn) {
+  getInn(inn,numInn);
+}
+scoreboard(getInningScore,inning, 12);
+
 
 
